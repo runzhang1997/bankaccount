06d9b63b8b3bb9f0c9a5af25b979c2e5dcf60f6d
runzhang <run.zhang@rwth-aachen.de>
2020-10-25 20:13:54 +0100

    final version bank transfer with test

---
+++ src/test/java/com.bearingpoint.codingexercise.bank/TransactionSystemTest.java
@@ -0,0 +1,8 @@
+package com.bearingpoint.codingexercise.bank;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TransactionSystemTest{
+    @Test
+    void
+}
